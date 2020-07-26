0d65c8abc8f28cfc11453d65fe445a025052d67d
orsu <theo181297@gmail.com>
2020-07-26 19:45:01 +0200

    removed comments, consoles.

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,6 @@ function App() {
 
       setTimeout(() => {
         if (coordObject) {
-          console.log(coordObject)
 
           let word = ""
 
@@ -61,7 +60,6 @@ function App() {
   }
 
   useEffect(() => {
-    //console.log(planchetteRef)
     if(planchetteRef.current && boardRef.current){
       planchetteRef.current.style.left = Math.random() * 100 + "%"
       planchetteRef.current.style.top = Math.random() * 100 + "%"
@@ -73,8 +71,6 @@ function App() {
     
 
   }, [])
-  
-  //let list = [];
 
   const movePlanchette = (coordX: number, coordY: number) => {
 
@@ -115,7 +111,6 @@ function App() {
     if (zone) {
       return zone
     } else {
-      //tolerance rate
       if (lowest && lowest < 0.06) {
         return closestPoint
       }
@@ -124,71 +119,11 @@ function App() {
     }
   }
 
-  /*
-  const handleDown = (event : React.MouseEvent<HTMLImageElement, MouseEvent>) => {
-    // const width = boardRef.current?.clientWidth;
-    // const height = boardRef.current?.clientHeight;
-    // if (width && height) {
-    //   const element = {
-    //     type:'char', 
-    //     char:'A', 
-    //     x:event.pageX/width, 
-    //     y:event.pageY/height,
-    //   }
-    //   list.push(element);
-    //   console.log(list);
-    // }
-
-    if(planchetteRef.current && boardRef.current){
-      const rect = boardRef.current.getBoundingClientRect();
-      //console.log(rect)
-      planchetteRef.current.style.left = (event.clientX-rect.left) + "px"
-      planchetteRef.current.style.top = (event.clientY-rect.top) + "px"
-    
-      let lowest: null|number = null
-      let zone: null|CoordinateType = null
-      let closestPoint: CoordinateType | null = null
-      const mouseX = (event.clientX-rect.left) / rect.width
-      const mouseY = (event.clientY-rect.top) / rect.height
-      coordinates.forEach(element => {
-        
-        if(element.type === "char"){
-
-          const distance = Math.sqrt( ((mouseX - element.x) * (mouseX - element.x)) + ((mouseY - element.y) * (mouseY - element.y)) )
-          if(lowest === null || distance < lowest) {
-
-            lowest = distance
-            closestPoint = element
-          }
-        } else if (element.type === "zone") {
-
-          if (mouseX > element.left && mouseY > element.top 
-            && mouseX < element.right && mouseY < element.bottom) {
-            zone = element
-          }
-          
-          
-        }
-      });
-
-      if (zone) {
-        console.log(zone)
-      } else {
-        if (lowest && lowest < 0.04) {
-          console.log(lowest)
-          console.log(closestPoint)
-        }
-      }
-    }
-  }
-  */
-
   return (
     <div className="App">
       <div className="container">
         <img 
           ref={boardRef}
-          //onMouseDown={handleDown} 
           className="board" 
           src={board} 
         />
@@ -197,16 +132,6 @@ function App() {
           className="planchette" 
           src={planchette} 
         />
-        {/* <div className="points">
-          {coordinates.map((element, index)=> element.type === "char" && (
-            <div key={index} className="point" style={{left:(element.x*100)+'%', top:(element.y*100)+'%'}}></div>
-          ))}
-        </div> */}
-        {/* <div className="zones">
-          {coordinates.map((element, index)=> element.type === "zone" && (
-            <div key={index} className="zone" style={{left:((element.left)*100)+'%', top:((element.top)*100)+'%', height:(element.bottom-element.top)*100+'%', width:(element.right-element.left)*100+'%'}}></div>
-          ))}
-        </div> */}
       </div>
       
     </div>
