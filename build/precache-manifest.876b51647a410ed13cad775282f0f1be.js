de7eb581634893c61771e4460befa7336ebc4c0b
orsu <theo181297@gmail.com>
2020-07-26 20:00:42 +0200

    added build

---
+++ build/precache-manifest.876b51647a410ed13cad775282f0f1be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf5bd38b40a7954f7f82b70d47119ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26afaf7d70dc698e3f04",
+    "url": "/static/css/main.f4e444c8.chunk.css"
+  },
+  {
+    "revision": "2c6f5d9872c61d2acd92",
+    "url": "/static/js/2.eb73c59e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eb73c59e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26afaf7d70dc698e3f04",
+    "url": "/static/js/main.0e90b85f.chunk.js"
+  },
+  {
+    "revision": "04e071afa4d1658c2fe9",
+    "url": "/static/js/runtime-main.3f23ea7d.js"
+  },
+  {
+    "revision": "b502bec63983281b46b2a9790e0e7727",
+    "url": "/static/media/Alain-Johannes-Ouija-Planchette.b502bec6.png"
+  },
+  {
+    "revision": "d1529b48584d7c1b44d06314c7adcd01",
+    "url": "/static/media/Ouija-main.d1529b48.jpg"
+  }
+]);
