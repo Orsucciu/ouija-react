cdf924b35d5e5835813f9cffa0093ed783a07052
orsu <theo181297@gmail.com>
2020-07-27 00:00:41 +0200

    Updates

---
+++ precache-manifest.5c974cc1d6dd61e5ec9916aa52c2cbb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866cd105cdad73875062309850317886",
+    "url": "/ouija-react/index.html"
+  },
+  {
+    "revision": "f85ae1f6c6f8971a8de2",
+    "url": "/ouija-react/static/css/main.039bf6b2.chunk.css"
+  },
+  {
+    "revision": "2c6f5d9872c61d2acd92",
+    "url": "/ouija-react/static/js/2.eb73c59e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ouija-react/static/js/2.eb73c59e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f85ae1f6c6f8971a8de2",
+    "url": "/ouija-react/static/js/main.fc067bf5.chunk.js"
+  },
+  {
+    "revision": "ad56429f8906a257cf4f",
+    "url": "/ouija-react/static/js/runtime-main.cf06960f.js"
+  },
+  {
+    "revision": "b502bec63983281b46b2a9790e0e7727",
+    "url": "/ouija-react/static/media/Alain-Johannes-Ouija-Planchette.b502bec6.png"
+  },
+  {
+    "revision": "d1529b48584d7c1b44d06314c7adcd01",
+    "url": "/ouija-react/static/media/Ouija-main.d1529b48.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f5b12346dd2cfa82fde37207715461b.js"
+  "/ouija-react/precache-manifest.5c974cc1d6dd61e5ec9916aa52c2cbb8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ouija-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
